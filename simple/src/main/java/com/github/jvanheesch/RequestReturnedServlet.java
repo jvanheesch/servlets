cd3ee58617b6e5b5ae0a395d9afa2ea264dcad3d
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-28 16:39:07 +0100

    Added RequestReturnedServlet.

---
+++ simple/src/main/java/com/github/jvanheesch/RequestReturnedServlet.java
@@ -0,0 +1,17 @@
+package com.github.jvanheesch;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class RequestReturnedServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write(new String(IOUtils.read(req.getInputStream())));
+        }
+    }
+}
