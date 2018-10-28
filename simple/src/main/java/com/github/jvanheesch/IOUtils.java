3a95d796c245ff670f39ef66ec4bc00bdf5d07bc
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-28 16:41:28 +0100

    Notes.

--- simple/src/main/java/com/github/jvanheesch/IOUtils.java
+++ simple/src/main/java/com/github/jvanheesch/IOUtils.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public final class IOUtils {
+    public static final int BUFFER_SIZE = 1024;
+
     private IOUtils() {
     }
 
     public static byte[] read(InputStream inputStream) throws IOException {
         try (ByteArrayOutputStream result = new ByteArrayOutputStream()) {
-            byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[BUFFER_SIZE];
             int length;
             while ((length = inputStream.read(buffer)) != -1) {
                 result.write(buffer, 0, length);
