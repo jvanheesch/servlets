c93d4316acac0fdf8dc3ca592c967eb959f58d29
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-28 13:47:17 +0100

    Added comment regarding wicket resources/IPackageResourceGuard.

--- wicket/src/main/java/com/github/jvanheesch/HelloWorldPage.java
+++ wicket/src/main/java/com/github/jvanheesch/HelloWorldPage.java
@@ -3,6 +3,7 @@ package com.github.jvanheesch;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
+// http://localhost:8100/wicket/wicket/resource/com.github.jvanheesch.HelloWorldPage/HelloWorldPage.html
 public class HelloWorldPage extends WebPage {
     private static final long serialVersionUID = 1662231773525391863L;
 
