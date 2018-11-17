2e9aac6895bce89533b3d5eb7c8bff7cd6a84070
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-11-17 13:43:34 +0100

    Made SleepyServlet async + changed logging levels.

--- async/src/main/java/com/github/jvanheesch/async/SleepyServlet.java
+++ async/src/main/java/com/github/jvanheesch/async/SleepyServlet.java
@@ -1,6 +1,9 @@
 package com.github.jvanheesch.async;
 
-import javax.servlet.ServletException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.AsyncContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,20 +11,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * An example of a blocking servlet.
+ * An example of an ASYNC servlet.
+ * The container is still blocking, however, due to the fact that server.tomcat.max-threads=1.
+ * The real work now happens in {@link AsyncContext#start(Runnable)}.
+ * See https://stackoverflow.com/questions/10073392/whats-the-purpose-of-asynccontext-start-in-servlet-3-0/
+ * and https://dzone.com/articles/limited-usefulness.
  */
-@WebServlet(name = "SleepyServlet", urlPatterns = "/sleepy")
+@WebServlet(name = "SleepyServlet", urlPatterns = "/sleepy", asyncSupported = true)
 public class SleepyServlet extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        long timeoutInMs = Long.parseLong(request.getParameter("timeoutInMs"));
-        try {
-            Thread.sleep(timeoutInMs);
-        } catch (InterruptedException e) {
-            throw new ServletException("Something went wrong...", e);
-        }
-        response.setContentType("text/html");
-        response.getWriter().println("God I love a good night's sleep.");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        LOGGER.info("Begin doGet, threadID: {}.", Thread.currentThread().getId());
+        AsyncContext asyncContext = request.startAsync();
+        asyncContext.start(() -> {
+            LOGGER.info("Begin sleep, threadID: {}.", Thread.currentThread().getId());
+            long timeoutInMs = Long.parseLong(request.getParameter("timeoutInMs"));
+            try {
+                Thread.sleep(timeoutInMs);
+                response.setContentType("text/html");
+                response.getWriter().println("God I love a good night's sleep.");
+                LOGGER.info("End sleep");
+                asyncContext.complete();
+            } catch (InterruptedException | IOException e) {
+                throw new RuntimeException("Something went wrong...", e);
+            }
+        });
+        LOGGER.info("End doGet");
     }
 }
