21ad965645ff7395f37c689ea570b84209425db7
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-11-18 16:27:43 +0100

    Filters now extend HttpFilter rather than implement Filter.

--- stack/src/main/java/com/github/jvanheesch/stack/Filter2.java
+++ stack/src/main/java/com/github/jvanheesch/stack/Filter2.java
@@ -3,17 +3,20 @@ package com.github.jvanheesch.stack;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class Filter2 implements Filter {
+public class Filter2 extends HttpFilter {
     private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         LOGGER.info("Filter2 - start.");
-        String path = ((HttpServletRequest) request).getRequestURI();
+        String path = request.getRequestURI();
         if (path.startsWith("/favicon")) {
             chain.doFilter(request, response); // Just continue chain.
         } else {
