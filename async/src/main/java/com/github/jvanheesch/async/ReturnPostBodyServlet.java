bb657752f77b6f452c4e79793ebb577da1bdd63b
jvanheesch <joris.vanheesch@ae.be>
2019-02-09 17:49:10 +0100

    Created ReturnPostBodyServlet.

---
+++ async/src/main/java/com/github/jvanheesch/async/ReturnPostBodyServlet.java
@@ -0,0 +1,29 @@
+package com.github.jvanheesch.async;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+// example: curl http://localhost:8080/post -d '{"abc": "def"}' -H "Content-Type: application/json"
+@WebServlet(name = "ReturnPostBodyServlet", urlPatterns = "/post")
+public class ReturnPostBodyServlet extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String postBody = request.getReader().lines().collect(Collectors.joining("\n"));
+            LOGGER.info("PostBody: {}.", postBody);
+            response.setContentType("text/html");
+            response.getWriter().println(postBody);
+        } catch (IOException e) {
+            throw new RuntimeException("Something went wrong...", e);
+        }
+    }
+}
